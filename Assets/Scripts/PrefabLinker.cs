1dd4d3f1e7cb564b42166208e49b25976c9fcd71
mdubbya <matt.aaron.whitehead@gmail.com>
2015-10-25 17:40:50 -0400

    -Changed prefab linker script to zero out the instantiated local position/ rotation, so it's original position is relative to the prefab to which it is linked. -Changed various prefabs to work with prefab linker script.

--- Assets/Scripts/PrefabLinker.cs
+++ Assets/Scripts/PrefabLinker.cs
@@ -9,8 +9,14 @@ public class PrefabLinker : MonoBehaviour
 	{
 		foreach (GameObject obj in LinkedPrefabs)
 		{
+			//Instantiate the object at at it's position
 			GameObject instantiatedObj = Instantiate(obj,obj.transform.position,obj.transform.rotation) as GameObject;
-			instantiatedObj.transform.SetParent(transform);
+			//Set object's parent as gameobject attached to this script
+			instantiatedObj.transform.parent=transform;
+			//Zero out local rotation/position so the it's location is relative to the
+			//gameobject attached to this script, instead of in world space
+			instantiatedObj.transform.localPosition = obj.transform.position;
+			instantiatedObj.transform.localRotation = obj.transform.rotation;
 		}
 	}
 }
