810131cb2b2ade0c21df69a614ac1aecc3d57b35
mdubbya <matt.aaron.whitehead@gmail.com>
2016-01-27 21:00:21 -0500

    Added physics utility to check ray collisions in a defined 'path'.

---
+++ Assets/Utilities/PhysicsUtilities.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{ 
+    public static class PhysicsUtilities
+    {
+        public static bool RayCastPath(Vector3 start, Vector3 direction, float width, float length, float rayCastIncrement, out RaycastHit rayCastHitInfo )
+        {
+            rayCastHitInfo = new RaycastHit();
+            Vector3 left = (Quaternion.Euler(0, 90, 0) * direction).normalized;
+            Vector3 right = (Quaternion.Euler(0, -90, 0) * direction).normalized;
+
+            float currentDistanceFromStart = width / 2;
+            Vector3 currentLeftVector = start + (left * (currentDistanceFromStart));
+            Vector3 currentRightVector = start + (right * (currentDistanceFromStart));
+
+            bool hitDetected = false;
+            //we will shoot vectors in [direction] until we detect a hit, or we've run out of vectors to shoot.
+            while((currentDistanceFromStart>0) && !hitDetected)
+            {
+                Debug.DrawRay(currentLeftVector, direction * length, Color.blue, 0.1f);
+                Debug.DrawRay(currentRightVector, direction * length, Color.blue, 0.1f);
+                if (Physics.Raycast(currentLeftVector, direction, out rayCastHitInfo, length))
+                {
+                    hitDetected = true;
+                    break;
+                }
+                else if (Physics.Raycast(currentRightVector, direction, out rayCastHitInfo, length))
+                {
+                    hitDetected = true;
+                    break;
+                }
+
+                currentDistanceFromStart -= rayCastIncrement;
+                currentLeftVector = start + (left * currentDistanceFromStart);
+                currentRightVector = start +(right * currentDistanceFromStart);
+            }
+
+            return hitDetected;
+        }
+    }
+}
