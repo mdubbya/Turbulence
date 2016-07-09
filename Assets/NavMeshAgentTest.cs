488c95cfbd07833ff506fb584246150f23639152
mdubbya <matt.aaron.whitehead@gmail.com>
2016-07-09 15:52:32 -0400

    -Initial Commit for NavMesh based AI navigation.

---
+++ Assets/NavMeshAgentTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshAgentTest : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+    private Rigidbody body;
+	// Use this for initialization
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        body = GetComponent<Rigidbody>();
+        agent.obstacleAvoidanceType = ObstacleAvoidanceType.HighQualityObstacleAvoidance;
+        agent.autoRepath = true;
+        agent.updatePosition = false;
+        agent.updateRotation = false;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+       
+        agent.SetDestination(target.position);
+        //body.AddForce(agent.desiredVelocity);
+    }
+
+    
+}
